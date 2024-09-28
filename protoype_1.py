830e0c09fb312e2c437f50bbedfbe55b63278574
ShawnDCS <shawnsanjayraj@gmail.com>
2024-09-28 20:48:32 +0800

    Update protoype_1.py

--- protoype_1.py
+++ protoype_1.py
@@ -1,14 +1,3 @@
 import streamlit as st
 
-# Set the title of the app
-st.title("My First Streamlit App")
-
-# Add a description
-st.write("Hello, welcome to my first Streamlit app!")
-
-# Create a simple input field
-user_input = st.text_input("Enter your name:")
-
-# Display the input back to the user
-if user_input:
-    st.write(f"Hello, {user_input}!")
+st.title = "Your name is shawn"
