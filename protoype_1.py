5de77f52f2d4de8e7debb98130a7013697741b58
ShawnDCS <shawnsanjayraj@gmail.com>
2024-09-28 21:41:27 +0800

    Update protoype_1.py

--- protoype_1.py
+++ protoype_1.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 
 # Header
 st.title("Top-Class Streamlit App")
@@ -9,24 +6,12 @@ st.markdown("Welcome to our interactive data visualization app!")
 
 # Sidebar navigation
 st.sidebar.header("Navigation")
-page = st.sidebar.selectbox("Select a page:", ["Home", "Data Visualization", "About"])
+page = st.sidebar.selectbox("Select a page:", ["Home", "About"])
 
 # Home Page
 if page == "Home":
     st.subheader("Home")
     st.write("This app showcases various data visualizations.")
-
-# Data Visualization Page
-elif page == "Data Visualization":
-    st.subheader("Data Visualization")
-    # Sample data
-    data = pd.DataFrame({
-        'x': np.linspace(0, 10, 100),
-        'y': np.sin(np.linspace(0, 10, 100))
-    })
-    fig, ax = plt.subplots()
-    ax.plot(data['x'], data['y'])
-    st.pyplot(fig)
 
 # About Page
 elif page == "About":
