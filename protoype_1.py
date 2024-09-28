7781dc73230cd6497d6a3d17f9efb9d69a5e0c63
ShawnDCS <shawnsanjayraj@gmail.com>
2024-09-28 21:39:12 +0800

    Update protoype_1.py

--- protoype_1.py
+++ protoype_1.py
@@ -1,6 +1,38 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
-st.title ("Stroke Prediction Website")
+# Header
+st.title("Top-Class Streamlit App")
+st.markdown("Welcome to our interactive data visualization app!")
 
-st.header ("Welcome to our Stroke Prediction website ")
+# Sidebar navigation
+st.sidebar.header("Navigation")
+page = st.sidebar.selectbox("Select a page:", ["Home", "Data Visualization", "About"])
 
+# Home Page
+if page == "Home":
+    st.subheader("Home")
+    st.write("This app showcases various data visualizations.")
+
+# Data Visualization Page
+elif page == "Data Visualization":
+    st.subheader("Data Visualization")
+    # Sample data
+    data = pd.DataFrame({
+        'x': np.linspace(0, 10, 100),
+        'y': np.sin(np.linspace(0, 10, 100))
+    })
+    fig, ax = plt.subplots()
+    ax.plot(data['x'], data['y'])
+    st.pyplot(fig)
+
+# About Page
+elif page == "About":
+    st.subheader("About")
+    st.write("This app was created to demonstrate the capabilities of Streamlit.")
+    
+# Footer
+st.markdown("---")
+st.markdown("© 2024 Your Name. All rights reserved.")
