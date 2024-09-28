d66adaf5263a9400bbe3795340007caebf4cb369
ShawnDCS <shawnsanjayraj@gmail.com>
2024-09-28 20:51:47 +0800

    Update protoype_1.py

--- protoype_1.py
+++ protoype_1.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title ("Stroke Prediction Website")
 
-st.header ("Welcome to our Stroke Prediction website !!")
+st.header ("Welcome to our Stroke Prediction website ")
 
